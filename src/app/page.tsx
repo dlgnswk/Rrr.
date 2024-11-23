e55e02099c731723944b71d921521764724dccea
ljhoon <dlwogns5140@gmail.com>
2024-11-23 21:22:05 +0900

    layout: homepage component abstract

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,16 @@
-import Image from "next/image";
-import { Separator } from "@/components/ui/separator";
+import posts from "@/constants/posts";
 
-export default function Home() {
+import HomepageTitle from "@/components/homepage/HomepageTitle";
+import HomepageLayout from "@/components/homepage/HomepageLayout";
+import HomepageArticle from "@/components/homepage/HomepageArticle";
+import HomepageFooter from "@/components/homepage/HomepageFooter";
+
+export default function Homepage() {
   return (
-    <div className="flex flex-col w-full items-start">
-      <h1 className="text-title font-honerThose">Rrr.</h1>
-      <main className="w-full h-full">
-        <div className="flex flex-col w-full gap-2">
-          <h1 className="text-5xl">
-            <span className="font-bold">리액트 쿼리</span>를 도입해 API
-            최적화하기
-          </h1>
-          <p className="font-light">
-            React Query V5를 사용하면서 배운 점들을 정리해보았다.
-          </p>
-        </div>
-        <Separator className="my-8 bg-black" />
-        <div className="flex flex-col w-full gap-2">
-          <h1 className="text-5xl">
-            <span className="font-bold">리액트 쿼리</span>를 도입해 API
-            최적화하기
-          </h1>
-          <p className="font-light">
-            React Query V5를 사용하면서 배운 점들을 정리해보았다.
-          </p>
-        </div>
-        <Separator className="my-8 bg-black" />
-        <div className="flex flex-col w-full gap-2">
-          <h1 className="text-5xl">
-            <span className="font-bold">리액트 쿼리</span>를 도입해 API
-            최적화하기
-          </h1>
-          <p className="font-light">
-            React Query V5를 사용하면서 배운 점들을 정리해보았다.
-          </p>
-        </div>
-        <Separator className="my-8 bg-black" />
-        <div className="flex flex-col w-full gap-2">
-          <h1 className="text-5xl">
-            <span className="font-bold">리액트 쿼리</span>를 도입해 API
-            최적화하기
-          </h1>
-          <p className="font-light">
-            React Query V5를 사용하면서 배운 점들을 정리해보았다.
-          </p>
-        </div>
-      </main>
-    </div>
+    <HomepageLayout>
+      <HomepageTitle title="Rrr." />
+      <HomepageArticle posts={posts} />
+      <HomepageFooter />
+    </HomepageLayout>
   );
 }
