9f2f00b2645294499e09c0b49eeebd5310e8ae42
Jaehoon Lee <dlwogns5140@gmail.com>
2024-11-23 22:41:09 +0900

    feat: page componenet add platejs

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,20 +1,38 @@
 "use client";
 
+import { PlateEditor } from "@/components/editor/plate-editor";
+import { SettingsProvider } from "@/components/editor/settings";
+
+import { Toaster } from "sonner";
+
 export default function WritePage() {
   const onSubmit = () => {};
 
   return (
-    <div className="flex flex-col mx-auto h-screen w-[80%]">
-      <div className="flex justify-between items-center gap-4">
-        <input
-          type="text"
-          className="w-full bg-sky h-10 border-b-2 border-black"
-        />
-        <button type="submit" onClick={onSubmit} className="font-honerThose">
-          SAVE
-        </button>
-      </div>
-      <form className="text-center w-full h-full"></form>
+    <div className="flex flex-col mx-auto max-w-[80%] min-h-screen p-4">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit();
+        }}
+      >
+        <div className="flex justify-between items-center gap-4 mb-4">
+          <input
+            type="text"
+            className="w-full bg-sky h-10 border-b-2 border-black"
+          />
+          <button type="submit" className="font-honerThose">
+            SAVE
+          </button>
+        </div>
+
+        <div className="h-screen w-full" data-registry="plate">
+          <SettingsProvider>
+            <PlateEditor />
+          </SettingsProvider>
+          <Toaster />
+        </div>
+      </form>
     </div>
   );
 }
