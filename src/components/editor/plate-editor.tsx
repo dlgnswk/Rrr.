6edb337562b349bda10db13ef483bce2c2f81a48
Jaehoon Lee <dlwogns5140@gmail.com>
2024-11-24 16:17:59 +0900

    design: toolbar style add

--- src/components/editor/plate-editor.tsx
+++ src/components/editor/plate-editor.tsx
@@ -9,17 +9,24 @@ import { Plate } from "@udecode/plate-common/react";
 import { useCreateEditor } from "@/components/editor/use-create-editor";
 import { SettingsDialog } from "@/components/editor/settings";
 import { Editor, EditorContainer } from "@/components/plate-ui/editor";
+import { Toolbar } from "@radix-ui/react-toolbar";
 
 export function PlateEditor() {
   const editor = useCreateEditor();
 
+  const handleChange = (content: any) => {
+    console.log(content);
+  };
+
   return (
     <DndProvider backend={HTML5Backend}>
-      <Plate editor={editor}>
+      <Plate editor={editor} onChange={handleChange}>
         <EditorContainer>
-          <Editor />
+          <Toolbar />
+          <div className="border border-input rounded-md">
+            <Editor className="min-h-[150px] focus:outline-none" />
+          </div>
         </EditorContainer>
-
         <SettingsDialog />
       </Plate>
     </DndProvider>
