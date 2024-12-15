17c99e76d887a583f3a4eed7d9db88975ff68b04
Jaehoon Lee <dlwogns5140@gmail.com>
2024-12-15 15:49:49 +0900

    refactor: remove unnecessary context

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -1,35 +1,30 @@
 import { prisma } from "@/lib/prisma";
 
-type Params = { params: { slug: string } };
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } }
+): Promise<Response> {
+  const { slug } = params;
 
-export async function GET(_: Request, context: Params): Promise<Response> {
   try {
     const post = await prisma.post.findUnique({
-      where: {
-        slug: context.params.slug,
-      },
+      where: { slug },
     });
 
     if (!post) {
       return new Response(JSON.stringify({ error: "Post not found" }), {
         status: 404,
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
       });
     }
 
     return new Response(JSON.stringify(post), {
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
     return new Response(JSON.stringify({ error: "Failed to fetch post" }), {
       status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
     });
   }
 }
