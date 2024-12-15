1776f54ddb583cd1e233b7f3d23242612d13dba8
Jaehoon Lee <dlwogns5140@gmail.com>
2024-12-15 15:59:58 +0900

    fix: update route for deploy error

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -1,26 +1,23 @@
-import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
 
 export async function GET(
-  request: Request,
-  { params }: { params: { slug: string } }
+  _: NextRequest,
+  context: { params: { slug: string } }
 ) {
   try {
     const post = await prisma.post.findUnique({
       where: {
-        slug: params.slug,
+        slug: context.params.slug,
       },
     });
 
     if (!post) {
-      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+      return Response.json({ error: "Post not found" }, { status: 404 });
     }
 
-    return NextResponse.json(post);
+    return Response.json(post);
   } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to fetch post" },
-      { status: 500 }
-    );
+    return Response.json({ error: "Failed to fetch post" }, { status: 500 });
   }
 }
