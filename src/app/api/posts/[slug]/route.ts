8cbabc5b37823383101ea97feae85f8d76c02334
Jaehoon Lee <dlwogns5140@gmail.com>
2024-12-15 15:35:20 +0900

    refactor: update GET API route types

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -1,26 +1,26 @@
 import { prisma } from "@/lib/prisma";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 
-export async function GET(
-  request: NextRequest,
-  { params }: { params: { slug: string } }
-) {
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function GET(request: NextRequest, context: Props) {
   try {
     const post = await prisma.post.findUnique({
       where: {
-        slug: params.slug,
+        slug: context.params.slug,
       },
     });
 
     if (!post) {
-      return NextResponse.json({ error: "Post not found" }, { status: 404 });
+      return Response.json({ error: "Post not found" }, { status: 404 });
     }
 
-    return NextResponse.json(post);
+    return Response.json(post);
   } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to fetch post" },
-      { status: 500 }
-    );
+    return Response.json({ error: "Failed to fetch post" }, { status: 500 });
   }
 }
