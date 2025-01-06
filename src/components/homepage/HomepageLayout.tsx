9cfdd0bdd03dacae868a746e7306f3530d93dfae
Jaehoon Lee <dlwogns5140@gmail.com>
2025-01-06 22:58:10 +0900

    design: 배경색 삭제

--- src/components/homepage/HomepageLayout.tsx
+++ src/components/homepage/HomepageLayout.tsx
@@ -4,7 +4,7 @@ export default function HomepageLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-sky">
+    <div>
       <main className="px-5 md:mx-auto w-full md:w-[80%] lg:w-[70%] xl:w-[60%] min-h-screen flex flex-col pt-10">
         {children}
       </main>
