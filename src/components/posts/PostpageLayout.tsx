dc8397732f0da91538be7d0b62fa041fdacf8906
Jaehoon Lee <dlwogns5140@gmail.com>
2025-01-08 22:44:23 +0900

    style: 불필요한 노드 및 스타일 삭제

--- src/components/posts/PostpageLayout.tsx
+++ src/components/posts/PostpageLayout.tsx
@@ -4,10 +4,8 @@ export default function PostpageLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-sky">
-      <main className="px-5 md:mx-auto w-full md:w-[80%] lg:w-[70%] xl:w-[60%] min-h-screen flex flex-col pt-10">
-        {children}
-      </main>
-    </div>
+    <main className="px-5 md:mx-auto w-full md:w-[80%] lg:w-[70%] xl:w-[60%] min-h-screen flex flex-col pt-10">
+      {children}
+    </main>
   );
 }
