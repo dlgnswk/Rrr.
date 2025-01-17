2932a74e0f2f57f95000fdddedb81749d887cb66
Jaehoon Lee <dlwogns5140@gmail.com>
2025-01-17 22:46:59 +0900

    style: plate editor 로직 삭제

--- src/app/posts/PostContent.tsx
+++ src/app/posts/PostContent.tsx
@@ -1,37 +1,10 @@
 "use client";
-import { HtmlReactPlugin } from "@udecode/plate-html/react";
 import { useGetPostBySlug } from "@/queries/useGetPostBySlug";
 import { Separator } from "@/components/ui/separator";
-import { createPlateEditor } from "@udecode/plate-common/react";
-import { useEffect, useState } from "react";
 import Profile from "@/components/posts/[slug]/profile";
-
-const editor = createPlateEditor({
-  plugins: [HtmlReactPlugin],
-});
 
 export default function PostContent({ slug }: { slug: string }) {
   const { data: post, isLoading, error } = useGetPostBySlug(slug);
-  const [html, setHtml] = useState("");
-
-  useEffect(() => {
-    if (!post?.content) {
-      setHtml("");
-      return;
-    }
-
-    Promise.resolve().then(() => {
-      try {
-        const serialized = editor.api.htmlReact.serialize({
-          nodes: post.content,
-        });
-        setHtml(serialized);
-      } catch (e) {
-        console.error("Serialization error:", e);
-        setHtml("");
-      }
-    });
-  }, [post?.content]);
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error loading post</div>;
@@ -65,10 +38,7 @@ export default function PostContent({ slug }: { slug: string }) {
         </div>
         <Separator className="my-6 bg-black" />
       </div>
-      <div
-        className="prose prose-slate prose-h1:text-4xl prose-h2:text-3xl max-w-none"
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
+      <div className="prose prose-slate prose-h1:text-4xl prose-h2:text-3xl max-w-none" />
     </article>
   );
 }
