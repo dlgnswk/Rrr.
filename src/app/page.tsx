ad64ada2abdb95975a1e21dc95f76118e61f9cd6
Jaehoon Lee <dlwogns5140@gmail.com>
2025-01-17 22:46:33 +0900

    feat: post list 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
+import { getAllPosts } from "@/app/utils/mdx";
 import HomepageTitle from "@/components/homepage/HomepageTitle";
 import HomepageLayout from "@/components/homepage/HomepageLayout";
-import HomepageArticle from "@/components/homepage/HomepageArticle";
 import HomepageFooter from "@/components/homepage/HomepageFooter";
-import { useGetPosts } from "@/queries/useGetPosts";
 import PostsList from "@/components/homepage/PostsList";
 
 export default async function Homepage() {
+  const posts = await getAllPosts();
+
   return (
     <HomepageLayout>
       <HomepageTitle />
-      <PostsList />
+      <PostsList posts={posts} />
       <HomepageFooter />
     </HomepageLayout>
   );
