486e230468c5c05444e8e177e509513ef417165a
Jaehoon Lee <dlwogns5140@gmail.com>
2025-01-22 22:59:52 +0900

    feat: mdx remote 연결

--- src/app/posts/PostContent.tsx
+++ src/app/posts/PostContent.tsx
@@ -1,7 +1,9 @@
 "use client";
+
 import { Separator } from "@/components/ui/separator";
 import Profile from "@/components/posts/[slug]/profile";
 import { Post } from "@/types/posts";
+import { MDXRemote } from "next-mdx-remote";
 
 export default function PostContent({ post }: { post: Post }) {
   return (
@@ -33,7 +35,7 @@ export default function PostContent({ post }: { post: Post }) {
         <Separator className="my-6 bg-black" />
       </div>
       <div className="prose prose-slate prose-h1:text-4xl prose-h2:text-3xl max-w-none">
-        {post.content}
+        <MDXRemote {...post.content} />
       </div>
     </article>
   );
