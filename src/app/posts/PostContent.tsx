4eb0c5d4d5dcd36d408d40c8b52c977480713b34
Jaehoon Lee <dlwogns5140@gmail.com>
2025-01-25 23:56:17 +0900

    feat: profile style 추가

--- src/app/posts/PostContent.tsx
+++ src/app/posts/PostContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Profile from "@/components/posts/[slug]/profile";
 import { Separator } from "@/components/ui/separator";
 import { Post } from "@/types/posts";
 import dynamic from "next/dynamic";
@@ -17,6 +18,25 @@ export default function PostContent({ post }: { post: Post }) {
           <span className="font-bold">{post.titleStyle.bold}</span>
           {post.titleStyle.rest}
         </h1>
+        <Separator className="my-6 bg-black" />
+        <div className="flex flex-col gap-1">
+          <div className="flex items-center gap-2">
+            <div className="w-10 h-10 text-foreground">
+              <Profile />
+            </div>
+            <span className="font-honerThose text-xl pt-2">Reeraeroon</span>
+          </div>
+          <div className="flex flex-col gap-1 ml-auto text-xs text-right">
+            <span className="font-honerThose">{post.publishedAt}</span>
+            <p className="flex gap-1">
+              <span>
+                {post.category}
+                {" | "}
+              </span>
+              <span>{post.tags.join(", ")}</span>
+            </p>
+          </div>
+        </div>
         <Separator className="my-6 bg-black" />
       </div>
       <MDXContent source={post.content} />
