fe8e1a5472be5c24d1e5a46ca32ea0aca009c4c7
Jaehoon Lee <dlwogns5140@gmail.com>
2025-01-30 23:52:59 +0900

    design: post 페이지 스타일 변경

--- src/app/posts/PostContent.tsx
+++ src/app/posts/PostContent.tsx
@@ -12,34 +12,37 @@ const MDXContent = dynamic(() => import("@/components/mdx/MDXContent"), {
 
 export default function PostContent({ post }: { post: Post }) {
   return (
-    <article className="flex flex-col w-full gap-3 mb-[100px] mt-[80px]">
-      <div className="flex flex-col">
-        <h1 className="text-5xl break-keep">
+    <article className="flex flex-col w-full gap-3 mb-[100px] mt-8">
+      <div className="flex flex-col gap-3">
+        <h1 className="text-5xl text-[#222a34] break-keep">
           <span className="font-bold">{post.titleStyle.bold}</span>
           {post.titleStyle.rest}
         </h1>
-        <Separator className="my-6 bg-black" />
-        <div className="flex flex-col gap-1">
-          <div className="flex items-center gap-2">
-            <div className="w-10 h-10 text-foreground">
-              <Profile />
-            </div>
-            <span className="font-honerThose text-xl pt-2">Reeraeroon</span>
+        <div className="flex items-center gap-2 mt-1">
+          <div className="w-[36px] h-[36px] text-foreground">
+            <Profile />
           </div>
-          <div className="flex flex-col gap-1 ml-auto text-xs text-right">
-            <span className="font-honerThose">{post.publishedAt}</span>
-            <p className="flex gap-1">
-              <span>
-                {post.category}
-                {" | "}
-              </span>
-              <span>{post.tags.join(", ")}</span>
-            </p>
+          <div className="flex flex-col justify-center text-[16px]">
+            <span className="font-semibold">Reeraeroon</span>
+            <span className="text-[12px] text-[#4F5968]">
+              {post.publishedAt}
+            </span>
           </div>
         </div>
-        <Separator className="my-6 bg-black" />
       </div>
       <MDXContent source={post.content} />
+      {post.tags && post.tags.length > 0 && (
+        <div className="w-full flex gap-2 mt-8">
+          {post.tags.map((tag) => (
+            <div
+              key={tag}
+              className="w-fit rounded-full bg-[#D9D9D9] bg-opacity-40 py-0.5 px-2.5 text-[12px] font-semibold text-[#4F5968]"
+            >
+              #{tag}
+            </div>
+          ))}
+        </div>
+      )}
     </article>
   );
 }
