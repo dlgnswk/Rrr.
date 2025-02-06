68c7c2e2cbdf93b622c5df39bc045096cf94b619
Jaehoon Lee <dlwogns5140@gmail.com>
2025-02-06 17:50:53 +0900

    design: title background color 삭제

--- src/components/posts/[slug]/MainButton.tsx
+++ src/components/posts/[slug]/MainButton.tsx
@@ -10,7 +10,7 @@ const MainButton = () => {
   };
 
   return (
-    <div className="flex w-full pt-4 pb-2 items-center bg-[#73BBE7]">
+    <div className="flex w-full pt-4 pb-2 items-center">
       <button
         className="flex items-center text-[30px] h-[36px] font-honerThose w-fit cursor-pointer"
         onClick={handleClick}
