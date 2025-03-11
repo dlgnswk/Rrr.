6e559d099119914cec7709a1925d23050ef3cd83
Jaehoon Lee <dlwogns5140@gmail.com>
2025-03-11 23:48:29 +0900

    feat: TagList 컴포넌트 추가

--- src/components/homepage/PostsList.tsx
+++ src/components/homepage/PostsList.tsx
@@ -1,5 +1,8 @@
 import { Post } from "@/types/posts";
+import { getTags } from "@/utils/tags";
+
 import HomepageArticle from "./HomepageArticle";
+import TagList from "./TagList";
 
 interface PostsListProps {
   posts: Post[];
@@ -8,6 +11,7 @@ interface PostsListProps {
 export default function PostsList({ posts }: PostsListProps) {
   return (
     <div>
+      <TagList tags={getTags(posts)} />
       <HomepageArticle posts={posts} />
     </div>
   );
