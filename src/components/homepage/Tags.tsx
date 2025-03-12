4cecdf1051adf5d4bd78b28df074c0560e6c1b78
Jaehoon Lee <dlwogns5140@gmail.com>
2025-03-12 11:17:11 +0900

    design: tag 컴포넌트 스타일 변경

--- src/components/homepage/Tags.tsx
+++ src/components/homepage/Tags.tsx
@@ -1,6 +1,6 @@
 export const TagSolidCircle = ({ tag }: { tag: string }) => {
   return (
-    <div className="w-fit flex items-center justify-center rounded-full border border-black bg-black py-0 px-3 text-[14px] font-medium text-white h-[21px]">
+    <div className="w-fit flex items-center justify-center rounded-full border border-black bg-black py-0 px-[16px] text-[16px] font-medium text-white h-[21px]">
       {tag}
     </div>
   );
@@ -8,7 +8,7 @@ export const TagSolidCircle = ({ tag }: { tag: string }) => {
 
 export const TagLineCircle = ({ tag }: { tag: string }) => {
   return (
-    <div className="w-fit flex items-center justify-center rounded-full border border-black py-0 px-[10px] text-[14px] font-medium text-black h-[21px]">
+    <div className="w-fit flex items-center justify-center rounded-full border border-black py-0 px-[12px] text-[16px] font-medium text-black h-[21px]">
       {tag}
     </div>
   );
@@ -16,7 +16,7 @@ export const TagLineCircle = ({ tag }: { tag: string }) => {
 
 export const TagSolidRectangle = ({ tag }: { tag: string }) => {
   return (
-    <div className="w-fit flex items-center justify-center border border-black bg-black py-0 px-[2px] text-[14px] font-medium text-white h-[21px]">
+    <div className="w-fit flex items-center justify-center border border-black bg-black py-0 px-[4px] text-[16px] font-medium text-white h-[21px]">
       {tag}
     </div>
   );
@@ -24,7 +24,7 @@ export const TagSolidRectangle = ({ tag }: { tag: string }) => {
 
 export const TagLineRectangle = ({ tag }: { tag: string }) => {
   return (
-    <div className="w-fit flex items-center justify-center border border-black py-0 px-[2px] text-[14px] font-medium text-black h-[21px]">
+    <div className="w-fit flex items-center justify-center border border-black py-0 px-[4px] text-[16px] font-medium text-black h-[21px]">
       {tag}
     </div>
   );
