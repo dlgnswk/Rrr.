9331e4db9eab1093f818b8332dcb491fd86cc929
Jaehoon Lee <dlwogns5140@gmail.com>
2025-03-14 17:54:16 +0900

    remove: shadcn/ui 의존성 제거

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -108,7 +108,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography")],
 } satisfies Config;
 
 export default config;
