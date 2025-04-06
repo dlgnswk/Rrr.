1df972ad6af848aa0aab9c0564e700beaa62b995
Jaehoon Lee <dlwogns5140@gmail.com>
2025-04-06 16:34:11 +0900

    design: 태그 리스트의 너비를 80%에서 500px로 변경

--- src/features/tag-list/ui/TagList.tsx
+++ src/features/tag-list/ui/TagList.tsx
@@ -4,7 +4,7 @@ import { getTagComponent } from "@/shared/model/tags";
 
 export default function TagList({ tags }: { tags: string[] }) {
   return (
-    <div className="flex gap-1 w-[80%] justify-center flex-wrap mb-16 mx-auto">
+    <div className="flex gap-1 w-[500px] justify-center flex-wrap mb-16 mx-auto">
       {tags.map((tag: string) => {
         const TagComponent = getTagComponent(tag);
         return <TagComponent key={tag} tag={tag} />;
