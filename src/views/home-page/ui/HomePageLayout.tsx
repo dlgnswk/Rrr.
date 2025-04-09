b0954269b81e457c886b5f8902150b1508cc7e18
Jaehoon Lee <dlwogns5140@gmail.com>
2025-04-09 23:20:58 +0900

    style: HomePageLayout의 main 태그에서 mx-auto 클래스를 추가하여 중앙 정렬 개선

--- src/views/home-page/ui/HomePageLayout.tsx
+++ src/views/home-page/ui/HomePageLayout.tsx
@@ -5,7 +5,7 @@ export default function HomePageLayout({
 }) {
   return (
     <div>
-      <main className="px-5 md:mx-auto  max-w-[900px] w-full min-h-screen flex flex-col pt-10">
+      <main className="px-5 mx-auto max-w-[900px] w-full min-h-screen flex flex-col pt-10">
         {children}
       </main>
     </div>
